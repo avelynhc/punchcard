209624fe39e5c9f9b48e9c435a8f157cc4efb898
lyndonoc <lyndonoc@gmail.com>
2021-09-21 22:59:02 -0400

    small improvement added

--- time_tracker.py
+++ time_tracker.py
@@ -1,15 +1,18 @@
 import sys
 
+def checkCommandName():
+    return sys.argv[1].lower() != 'start' and sys.argv[1].lower() != 'finish'
+
 if len(sys.argv) == 1:
     print('you must pass 2 arguments')
     sys.exit()
 elif len(sys.argv) == 2:
-    if sys.argv[1].lower() != 'start' and sys.argv[1].lower() != 'finish':
+    if checkCommandName():
         sys.exit('it must be start or finish')
     else:
        sys.exit('it must have project name')
 elif len(sys.argv) == 3:
-    if sys.argv[1].lower() != 'start' and sys.argv[1].lower() != 'finish':
+    if checkCommandName():
         sys.exit('it must be start or finish')
 
 print(sys.argv[1] + "ing ", sys.argv[2] + "...")
