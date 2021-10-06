e2ea3e02a25a40fe3c0271372c0f31d5dfe473a3
Hyunjeong Choi <hchoi72@myseneca.ca>
2021-10-06 09:17:02 -0400

    added same finish warning

--- time_tracker.py
+++ time_tracker.py
@@ -14,6 +14,11 @@ def has_duplicate_task(task, task_name):
             return True
     return False
 
+def has_finish(task, finish):
+    for x in task:
+        if finish not in x: 
+            return False
+    return True
 
 if len(sys.argv) == 1:
     print('you must pass 2 arguments')
@@ -36,20 +41,31 @@ if os.path.isfile(filePath) and os.access(filePath, os.R_OK):
     # read json
     with open(filePath) as json_file:
         data = json.load(json_file)
+    if sys.argv[1] == 'start':
+            if has_duplicate_task(data["tasks"], sys.argv[2]):
+                sys.exit('You already started this project')
 
+            with open(filePath, 'w', encoding='utf-8') as f:
+                dest = {
+                    sys.argv[2]: [
+                        {
+                            sys.argv[1]: time.time()
+                        }
+                    ]
+                }
+                data["tasks"].update(dest)
+                json.dump(data, f, ensure_ascii=False, indent=2)
+    elif sys.argv[1] == 'finish':
         if has_duplicate_task(data["tasks"], sys.argv[2]):
-            sys.exit('You already started this project')
+            # print(data["tasks"]["watermelon"][0])
+            if has_finish(data["tasks"]["watermelon"],sys.argv[1]):
+                sys.exit("You do not have unfinished project named " + sys.argv[2])
+            else:
+                print("adding finishing time")
 
-        with open(filePath, 'w', encoding='utf-8') as f:
-            dest = {
-                sys.argv[2]: [
-                    {
-                        sys.argv[1]: time.time()
-                    }
-                ]
-            }
-            data["tasks"].update(dest)
-            json.dump(data, f, ensure_ascii=False, indent=2)
+        else:
+            sys.exit("You do not have unfinished project named " + sys.argv[2])
+
 
 else:
     print("Either file is missing or is not readable, creating file...")
