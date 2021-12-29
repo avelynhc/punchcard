9e5b3b5b4baf58d4e6ec3a4b6e7100ef1449fe0c
lyndonoc <lyndonoc@gmail.com>
2021-12-28 22:13:28 -0500

    added unit tests for task_manager

--- lib/task_manager.py
+++ lib/task_manager.py
@@ -5,6 +5,11 @@ from lib.task_list import TaskList
 
 
 class TaskManager:
+    start_success_msg = "successfully started {}"
+    finish_success_msg = "successfully finished {}"
+    task_not_found_msg = "Error: no task named {} found"
+    cancel_success_msg = "successfully cancelled {}"
+
     data_file_path: str
     tasks: Dict[str, TaskList]
 
@@ -20,25 +25,42 @@ class TaskManager:
         self.tasks = {}
 
     def start_task(self, task_name: str) -> str:
-        self.save_data()
-        return "successfully started {}".format(task_name)
+        # self.save_data()
+        if task_name in self.tasks.keys():
+            try:
+                task_list = self.tasks[task_name]
+                task_list.start_new_task()
+            except Exception as err:
+                return "Error: {}".format(err)
+        else:
+            new_task_list = TaskList()
+            new_task_list.start_new_task()
+            self.tasks[task_name] = new_task_list
+        return self.start_success_msg.format(task_name)
 
     def finish_task(self, task_name: str) -> str:
-        if not self.has_task(task_name):
-            raise Exception("no task named {} found".format(task_name))
-        self.save_data()
-        return "successfully finished {}".format(task_name)
+        # self.save_data()
+        if task_name not in self.tasks.keys():
+            return self.task_not_found_msg.format(task_name)
+        try:
+            task_list = self.tasks[task_name]
+            task_list.finish_task()
+        except Exception as err:
+            return "Error: {}".format(err)
+        return self.finish_success_msg.format(task_name)
 
     def cancel_task(self, task_name: str) -> str:
-        if not self.has_task(task_name):
-            raise Exception("no task named {} found".format(task_name))
-        self.save_data()
-        return "successfully canceled {}".format(task_name)
+        # self.save_data()
+        if task_name not in self.tasks.keys():
+            return self.task_not_found_msg.format(task_name)
+        try:
+            task_list = self.tasks[task_name]
+            task_list.cancel_task()
+        except Exception as err:
+            return "Error: {}".format(err)
+        return self.cancel_success_msg.format(task_name)
 
     def get_task(self, task_name: str, from_ts: int, to_ts: int) -> str:
-        if not self.has_task(task_name):
-            raise Exception("no task named {} found".format(task_name))
+        if task_name not in self.tasks.keys():
+            return self.task_not_found_msg.format(task_name)
         return self.tasks[task_name].get_duration(from_ts, to_ts)
-
-    def has_task(self, task_name):
-        return True
