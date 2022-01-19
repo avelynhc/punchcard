13b34b947c6568e3b162e14813fb63f35245154c
lyndonoc <lyndonoc@gmail.com>
2022-01-18 20:56:51 -0500

    dockerized Flask and set up docker compose with DB

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,16 @@
 import os
+
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/ping", methods=["GET"])
-def get_ping():
-    return "pong"
+@app.route('/ping', methods=["GET"])
+def ping_pong():
+    return 'pong'
 
-if __name__ == "__main__":
-    app.run(port=os.getenv("PORT", 4000), debug=os.getenv("DEBUG", "false") == "true")
+if __name__ == '__main__':
+    app.run(
+        debug=os.environ.get("DEBUG", default="false") == "true",
+        host="0.0.0.0",
+        port=os.environ.get('PORT', default=5000),
+    )
