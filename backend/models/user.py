064f04fb8cae5816a41f3d6c427ddfddb5fdb783
avelynhc <avelynhc@gmail.com>
2022-01-20 21:48:24 -0500

    modified user.py for user register/login

--- backend/models/user.py
+++ backend/models/user.py
@@ -4,4 +4,18 @@ class UserModel(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    provider_id = db.Column(db.String)
+    username = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+
+    def __init__(self, username, password):
+        self.username = username
+        # TODO: password should be hashed
+        self.password = password
+    
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def find_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
