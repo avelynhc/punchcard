218b7ef09395b73211d5f495875fb06d09e063f2
Hyunjeong Choi <75185537+avelynhc@users.noreply.github.com>
2022-01-26 11:37:55 -0500

    added hashing password function (#17)
    * added hassing password function
    
    * removed extra line

--- backend/resources/user.py
+++ backend/resources/user.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource, reqparse
 from werkzeug.security import safe_str_cmp
 from flask_jwt_extended import create_access_token
+from flask_bcrypt import Bcrypt
 
 from models.user import UserModel
 
@@ -31,9 +32,12 @@ class UserLogin(Resource):
     def post(self):
         data = _user_parser.parse_args()
         user = UserModel.find_by_username(data["username"])
-        if user and safe_str_cmp(user.password, data["password"]):
+        bcrypt = Bcrypt()
+        if user and bcrypt.check_password_hash(user.password, data["password"]):
             access_token = create_access_token(identity=user.id)
             return {
-                    'access_token': access_token
+                    "access_token": access_token
             }, 200
         return {"message": "Invalid credentials"}, 401
+
+
