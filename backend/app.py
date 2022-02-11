08441fbf985d3b9c63ba9958f81ccb40b34bd7dd
Hyunjeong Choi <75185537+avelynhc@users.noreply.github.com>
2022-02-10 21:29:56 -0500

    added new endpoint for duration (#24)

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
 
 from db.db import db
 from resources.user import UserRegister, UserLogin, UserModel
-from resources.task_detail import TaskDetail, TaskDetailList, TaskDetailWithFinish, TaskDetailWithCancel
+from resources.task_detail import TaskDetail, TaskDetailList, TaskDetailWithFinish, TaskDetailWithCancel, TaskDuration
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
@@ -49,6 +49,7 @@ def missing_token_callback(error):
 
 api.add_resource(UserRegister, "/register")
 api.add_resource(UserLogin, "/login")
+api.add_resource(TaskDuration, "/task/<string:task_name>/duration")
 api.add_resource(TaskDetail, "/task/<string:task_name>")
 api.add_resource(TaskDetailWithFinish, "/task/<string:task_name>/finish")
 api.add_resource(TaskDetailWithCancel, "/task/<string:task_name>/cancel")
