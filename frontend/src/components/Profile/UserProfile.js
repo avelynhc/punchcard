697bb10c7a5c72ca7256a5709fda8583cd12c8c0
avelynhc <avelynhc@gmail.com>
2022-05-10 15:04:43 +0900

    added functionality to display duration

--- frontend/src/components/Profile/UserProfile.js
+++ frontend/src/components/Profile/UserProfile.js
@@ -5,10 +5,8 @@ const BACKEND_API = "http://127.0.0.1:4000";
 
 const UserProfile = () => {
   const [tasks, setTasks] = useState([]);
-  const [durations, setDurations] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [durationError, setDurationError] = useState(null);
 
   const fetchTasksHandler = useCallback(async () => {
     setIsLoading(true);
@@ -25,39 +23,43 @@ const UserProfile = () => {
           throw new Error("Cannot fetch task detail");
         }
         const data = await response.json();
-        setTasks(data);
+        for (let i = 0; i < data.task_detail.length; i++) {
+          if (data.task_detail[i].finish_time) {
+            const current_duration = await FetchDurationHandler(
+              data.task_detail[i].task_name
+            );
+            data.task_detail[i].duration = current_duration.duration;
+          }
+        }
+        setTasks(data.task_detail);
       }
     } catch (error) {
+      console.log(error);
       setError(error.message);
     }
     setIsLoading(false);
   }, []);
 
-  const fetchDurationHandler = useCallback(async (taskName) => {
-    // setIsLoading(true);
-    setDurationError(null);
-    try {
-      let token = localStorage.getItem("token");
-      if (token) {
-        const response = await fetch(
-          `${BACKEND_API}/task/${taskName}/duration`,
-          {
-            headers: {
-              Authorization: "Bearer " + token,
-            },
+  const FetchDurationHandler = (taskName) => {
+    let token = localStorage.getItem("token");
+    if (token) {
+      return fetch(`${BACKEND_API}/task/${taskName}/duration`, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      })
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          } else {
+            throw new Error("Cannot fetch task duration");
           }
-        );
-        if (!response.ok) {
-          throw new Error("Cannot fetch task duration");
-        }
-        const durationData = await response.json();
-        setDurations(durationData);
-      }
-    } catch (error) {
-      setDurationError(error.message);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
-    // setIsLoading(false);
-  }, [durationError]);
+  };
 
   useEffect(() => {
     fetchTasksHandler();
@@ -71,29 +73,30 @@ const UserProfile = () => {
         <section>
           <button onClick={fetchTasksHandler}>Fetch Task Details</button>
         </section>
-        {isLoading ? <p>Loading...</p> : ""}
-        {error ? <p className={classes.error}>{error}</p> : ""}
-        <ul className={classes.data}>
-          {tasks.task_detail && tasks.task_detail.length > 0 ? (
-            tasks.task_detail.map((task) => (
+        {isLoading && <p>Loading...</p>}
+        {error && <p className={classes.error}>{error}</p>}
+
+        {tasks.length > 0 ? (
+          <ul className={classes.data}>
+            {tasks.map((task) => (
               <li key={task.id}>
                 <p>task name: {task.task_name}</p>
                 <p>start time: {task.start_time}</p>
                 {task.finish_time && <p>finish time: {task.finish_time}</p>}
+
+                {task.duration && <p>duration: {task.duration}</p>}
+
                 {task.finish_time ? (
                   <p className={classes.complete}>Completed</p>
                 ) : (
                   <p className={classes.pending}>Not Completed</p>
                 )}
-                {task.finish_time && durations.duration && (
-                  <p>duration: {fetchDurationHandler(task.task_name)}</p>
-                )}
               </li>
-            ))
-          ) : (
-            <p>Found no tasks</p>
-          )}
-        </ul>
+            ))}
+          </ul>
+        ) : (
+          <p>Found no tasks</p>
+        )}
       </section>
     </React.Fragment>
   );
